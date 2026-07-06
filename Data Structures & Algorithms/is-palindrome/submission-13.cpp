9be44da0f8aa278e1132e6f7066f8c2247998188
lordsuirules-blip <lordsuirules@gmail.com>
2026-07-06 18:48:31 +0530

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    int left = 0;
+    int right = s.size() - 1;
+
+    while(left < right){
+        while(left < right && !isalnum(s[left]))
+            left++;
+
+        while(left < right && !isalnum(s[right]))
+            right--;
+            
+        if(toupper(s[left]) == toupper(s[right])){
+            left++;
+            right--;
+        }else{
+            return false;
+        }
+    }
+    return true;
+    }
+};
