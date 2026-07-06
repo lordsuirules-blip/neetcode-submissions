44dc5fb11f0e3fd162666da9bb671115adac1963
lordsuirules-blip <lordsuirules@gmail.com>
2026-07-06 16:37:27 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    unordered_map<char, int> mp(s.size());
+
+    for (auto i : s)
+        mp[i]++;
+
+    if (t.size() != s.size())
+        return false;
+
+    for (auto i : t)
+    {
+        mp[i]--;
+        if (mp[i] < 0)
+            return false;
+    }
+
+    return true;
+
+    }
+};
