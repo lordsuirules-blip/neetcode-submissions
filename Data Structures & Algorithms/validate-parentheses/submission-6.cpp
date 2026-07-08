c5c4b75c10b1face4d6ab9df2cd739adda7dedf9
lordsuirules-blip <lordsuirules@gmail.com>
2026-07-08 17:06:29 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> mp{
+            {'}', '{'},
+            {']', '['},
+            {')', '('},
+        };
+
+        for (char c : s) {
+            if (c == '{' || c == '[' || c == '(') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+
+                if (st.top() == mp[c])
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
