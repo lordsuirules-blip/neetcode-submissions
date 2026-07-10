f001d8963d602959482643e03bc0cc23273b3e2f
lordsuirules-blip <lordsuirules@gmail.com>
2026-07-10 20:07:12 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+   public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> v;
+
+        for (const string& word : strs) {
+            vector<int> freq(26, 0);
+
+            for (char c : word) {
+                freq[c - 'a']++;
+            }
+
+            string key;
+
+            for (int i : freq) {
+                key += to_string(i) + "#";
+            }
+
+            mp[key].emplace_back(word);
+        }
+
+        for (auto& it : mp) {
+            v.emplace_back(it.second);
+        }
+
+        return v;
+    }
+};
