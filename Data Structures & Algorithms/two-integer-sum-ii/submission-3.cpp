868ddac2e8021f406bb071e2959a23502068b2e3
lordsuirules-blip <lordsuirules@gmail.com>
2026-07-13 18:11:29 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        // unordered_map<int, int> mp;
+
+        int l = 0;
+        int r = numbers.size() - 1;
+
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target)
+                break;
+            else if (sum < target)
+                l++;
+            else
+                r--;
+        }
+
+        ans.emplace_back(l + 1);
+        ans.emplace_back(r + 1);
+
+        return ans;
+    }
+};
